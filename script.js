50341b5da494746513ddf5ebaf738b50125e2a97
leandro <leandrinhuugp@gmail.com>
2026-09-16 18:38:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -66,6 +66,18 @@ if (productForm) {
             return;
         }
 
+        const products = getProducts();
+
+        const productExist = products.some(function (product) {
+            return product.name.toLowerCase() === name.toLowerCase();
+        });
+
+        if (productExist) {
+            alert("Este produto já está cadastrado no estoque. \nEdite as informações como nome e preço de um produto existente na aba Produtos.");
+            productInput.focus();
+            return;
+        }
+
         const product = {
             id: Date.now(),
             name: name,
@@ -73,9 +85,7 @@ if (productForm) {
             amount: amount
         };
 
-        const products = getProducts();
-
-        products.push(product);
+        products.unshift(product);
 
         saveProducts(products);
 
@@ -85,4 +95,174 @@ if (productForm) {
     });
 }
 
+// FORMARTAR MOEDA PARA BRL
+function formatBRL(value) {
+    return value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+}
+
 // EXIBIR PRODUTOS
+const productsList = document.querySelector(".products-list");
+
+if (productsList) {
+    const products = getProducts();
+    
+    productsList.innerHTML = "";
+
+    if (products.length === 0) {
+        productsList.innerHTML = `
+            <div class="empty-products">
+                <h2>Nenhum produto cadastrado</h2>
+                <p>Adicione um produto para começar seu estoque.</p>
+            </div>
+        `;
+    } else {
+        products.forEach(function (product) {
+            const productCard = document.createElement("div");
+            productCard.classList.add("product-card");
+
+            productCard.innerHTML = `
+                <div class="product-info">
+                    <h2 class="product-name">${product.name}</h2>
+
+                    <div class="product-details">
+                        <span class="product-price">
+                            Preço: <strong>${formatBRL(product.price)}</strong>
+                        </span>
+
+                        <span class="product-amount">
+                            Quantidade: <strong>${product.amount}</strong>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="product-actions">
+                    <button
+                        class="product-btn edit-btn"
+                        type="button"
+                        data-id="${product.id}">
+                        Editar
+                    </button>
+
+                    <button
+                        class="product-btn delete-btn"
+                        type="button"
+                        data-id="${product.id}">
+                        Retirar
+                    </button>
+                </div>
+            `;
+            
+            productsList.appendChild(productCard);
+        });
+    }
+    updateSummary();
+}
+
+// SUMARIO
+function updateSummary() {
+    const totalProductsElement = document.querySelector(".summary-card:nth-child(1) .summary-value");
+    const totalValueElement = document.querySelector(".summary-card:nth-child(2) .summary-value");
+
+    if (!totalProductsElement || !totalValueElement) {
+        return;
+    }
+
+    const products = getProducts();
+
+    const totalAmount = products.reduce(function (total, product) {
+        return total + product.amount;
+    }, 0);
+
+    const totalValue = products.reduce(function (total, product) {
+        return total + (product.price * product.amount);
+    }, 0);
+
+    totalProductsElement.textContent = totalAmount;
+    totalValueElement.textContent = formatBRL(totalValue);
+}
+
+// EDITAR PRODUTO
+
+document.addEventListener("click", function(event) {
+    if (!event.target.classList.contains("edit-btn")) {
+        return;
+    }
+
+    const id = Number(event.target.dataset.id);
+    const products = getProducts();
+
+    const product = products.find(function(product) {
+        return product.id === id;
+    });
+
+    if (!product) {
+        return;
+    }
+
+    let newName;
+    while (true){
+        newName = prompt(
+            "Nome do produto",
+            product.name
+        );
+
+        if (newName === null) {
+            return;
+        }
+
+        if (newName.trim() !== "") {
+            break;
+        }
+        alert("Digite um nome válido.");
+    }
+
+    let newPrice;
+    let price;
+    while (true) {
+        newPrice = prompt(
+            "Preço do produto:",
+            product.price
+        );
+
+        if (newPrice === null) {
+            return;
+        }
+
+        price = Number(newPrice);
+        if (!isNaN(price) && price > 0) {
+            break;
+        }
+
+        alert("Digite um preço válido.");
+    }
+
+    let addAmount;
+    let amountToAdd;
+    while (true) {
+        addAmount = prompt(
+            "Quantidade atual: " + product.amount + "\n\n" + "Quantas unidades deseja adicionar? \n(Mantenha o campo vazio caso não queira adicionar.)"
+        );
+
+        if (addAmount === null) {
+            return;
+        }
+
+        amountToAdd = Number(addAmount);
+        if (!isNaN(amountToAdd) && amountToAdd >= 0){
+            break;
+        }
+
+        alert("Digite uma quantidade válida.");
+    }
+
+    product.name = newName.trim();
+    product.price = price;
+    product.amount += amountToAdd;
+
+    saveProducts(products);
+    alert("Produto atualizado com sucesso!");
+    this.location.reload();
+});
